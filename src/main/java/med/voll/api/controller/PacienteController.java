9c8be866b8343ef0889480e079d9920476aab628
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-08-07 22:42:37 -0600

    add valñidated

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,17 +1,27 @@
 package med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import med.voll.api.paciente.DatosRegistroPaciente;
+import med.voll.api.paciente.PaacienteRepository;
+import med.voll.api.paciente.Paciente;
 
 @RestController
-@RequestMapping("/pacientes")
+@RequestMapping("pacientes")
 public class PacienteController {
+
+    @Autowired
+    private PaacienteRepository repository;
+
     @PostMapping
-    public void registrar(@RequestBody DatosRegistroPaciente datos){
-        System.out.println("Datos recibidos: " + datos);
+    @Transactional
+    public void registrar(@RequestBody @Valid DatosRegistroPaciente datos){
+        repository.save(new Paciente(datos));
     }
 }
