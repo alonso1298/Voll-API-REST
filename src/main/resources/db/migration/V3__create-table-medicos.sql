843d0b145cc6b4e996f086238eaac35b4f3c49e7
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-08-07 22:46:07 -0600

    refactor

--- src/main/resources/db/migration/V3__create-table-medicos.sql
+++ src/main/resources/db/migration/V3__create-table-medicos.sql
@@ -1,17 +1,16 @@
-CREATE TABLE medicos(
-    
-    id bigint NOT NULL auto_increment,
-    nombre varchar(100) NOT NULL,
-    email varchar(100) NOT NULL UNIQUE,
-    documento varchar(12) NOT NULL UNIQUE,
-    especialidad varchar(100) NOT NULL,
-    calle varchar(100) NOT NULL,
-    barrio varchar(100) NOT NULL,
-    codigo_postal varchar(12) NOT NULL,
-    complemento varchar(100),
-    numero varchar(20),
-    estado char(100) NOT NULL,
-    ciudad varchar(100) NOT NULL,
+create table pacientes( 
+    id bigint not null auto_increment, 
+    nombre varchar(100) not null, 
+    email varchar(100) not null unique, 
+    documento_identidad varchar(14) not null unique, 
+    telefono varchar(20) not null, 
+    calle varchar(100) not null, 
+    numero varchar(100), 
+    complemento varchar(100), 
+    barrio varchar(100) not null, 
+    ciudad varchar(100) not null, 
+    estado varchar(100) not null, 
+    codigo_postal varchar(9) not null, 
 
-    PRIMARY KEY(id)
+    primary key(id) 
 );
