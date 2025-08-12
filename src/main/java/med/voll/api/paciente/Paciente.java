758d347b71e77c39b0711f270d1176f016beeeb9
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-08-11 22:37:56 -0600

    add methos and DTO

--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -26,16 +26,29 @@ public class Paciente {
     private String email;
     private String documentoIdentidad;
     private String telefono;
+    private boolean activo;
 
     @Embedded
     private Direccion direccion;
 
     public Paciente(DatosRegistroPaciente datos){
+        this.activo = true;
         this.nombre = datos.nombre();
         this.email = datos.email();
         this.documentoIdentidad = datos.documentoIdentidad();
         this.telefono = datos.telefono();
         this.direccion = new Direccion(datos.direccion());
+    }
+
+    public void actualizarInformacion(DatosActualizacionPaciente datos){
+        if (datos.nombre() != null) 
+        this.nombre = datos.nombre(); 
+    
+        if (datos.telefono() != null) 
+            this.telefono = datos.telefono(); 
+
+        if (datos.direccion() != null) 
+            direccion. actualizarDireccion(datos.direccion()); 
     }
 
     public void desactivar() {
