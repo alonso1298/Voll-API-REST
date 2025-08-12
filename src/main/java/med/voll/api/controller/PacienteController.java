4613b22bcb534aed413346c0f9b480ddc3f02975
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-08-11 19:19:20 -0600

    Add DTO and method in paciente

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,13 +1,18 @@
 package med.voll.api.controller;
 
+import org.springframework.data.domain.Page;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+import med.voll.api.paciente.DatosListaPaciente;
 import med.voll.api.paciente.DatosRegistroPaciente;
 import med.voll.api.paciente.PaacienteRepository;
 import med.voll.api.paciente.Paciente;
@@ -23,5 +28,10 @@ public class PacienteController {
     @Transactional
     public void registrar(@RequestBody @Valid DatosRegistroPaciente datos){
         repository.save(new Paciente(datos));
+    }
+
+    @GetMapping
+    public Page<DatosListaPaciente> listar(@PageableDefault(size = 10, sort = {"nombre"}) Pageable paginacion) {
+        return repository.findAll(paginacion).map(DatosListaPaciente::new);
     }
 }
---
+++ src/main/java/med/voll/api/paciente/DatosListaPaciente.java
@@ -0,0 +1,17 @@
+package med.voll.api.paciente;
+
+
+public record DatosListaPaciente(
+    String nombre,
+    String email,
+    String telefono,
+    String documentoIdentidad
+) {
+    public DatosListaPaciente(Paciente paciente){
+        this(
+            paciente.getNombre(), 
+            paciente.getEmail(), 
+            paciente.getTelefono(), 
+            paciente.getDocumentoIdentidad());
+    }
+}
