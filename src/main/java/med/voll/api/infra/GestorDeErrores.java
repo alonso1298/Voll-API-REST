3a7f2c0e05f51f309608a6d8d22fa9552eaacded
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-08-12 20:01:04 -0600

    Tratando el error 400

--- src/main/java/med/voll/api/infra/GestorDeErrores.java
+++ src/main/java/med/voll/api/infra/GestorDeErrores.java
@@ -1,6 +1,8 @@
 package med.voll.api.infra;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,5 +14,20 @@ public class GestorDeErrores {
     @ExceptionHandler(EntityNotFoundException.class) // Cuando halla un error de tipo EntityNotFoundException se llamara al metodo gestionarError404
     public ResponseEntity gestionarError404(){
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class) // Cuando halla un error de tipo EntityNotFoundException se llamara al metodo gestionarError404
+    public ResponseEntity gestionarError400(MethodArgumentNotValidException ex){
+        var errores = ex.getFieldErrors(); // Nos devuelve todos los errores dentro de esa variable llamada errores
+        return ResponseEntity.badRequest().body(errores.stream().map(DatosErrorValidacion::new).toList());
+    }
+
+    public record DatosErrorValidacion(
+        String campo,
+        String mensaje
+    ) {
+        public DatosErrorValidacion(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
