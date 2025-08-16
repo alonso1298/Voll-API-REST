ade3b0bb6fa1f77dd3c6f0222b0c256374357384
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-08-15 20:31:58 -0600

    Validando el Token recibido

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -2,6 +2,7 @@ package med.voll.api.infra.security;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,10 +14,13 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component // Component es algo generico que necesitamos que Spring lo cargue
 public class SecurityFilter extends OncePerRequestFilter{
 
+    @Autowired
+    private TokenService tokenService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJWT = recuperarToken(request);
-        System.out.println(tokenJWT);
+        var subject = tokenService.getSubject(tokenJWT);
         filterChain.doFilter(request, response);
     }
 
