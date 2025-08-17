ab897d3278255f23f582812d18d5f93dcea172ce
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-08-16 18:14:46 -0600

    Add consulta Controller

---
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -0,0 +1,22 @@
+package med.voll.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("consultas")
+public class ConsultaController {
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity reservar(@RequestBody @Valid DatosReservaConsulta datos){
+        System.out.println(datos);
+        return ResponseEntity.ok(new DatosdetalleConsulta())
+    }
+}
