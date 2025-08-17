95e8b696a522b59a4238453002359cffa869c316
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-08-16 21:20:38 -0600

    add ValidarPacienteActivo

--- src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
+++ src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface PacienteRepository extends JpaRepository<Paciente, Long>{
     // Con esto tenemos un set de metodos automaticos que incluyen toda la parte de un CRUD
     
     Page<Paciente> findAllByActivoTrue(Pageable paginacion);
+
+    boolean findActivoById(Long idPaciente);
 }
