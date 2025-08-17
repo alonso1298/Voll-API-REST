96d5b95b55aac48ca620e2a2ba7531000e221215
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-08-17 15:47:06 -0600

    JWT en la documentacion

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import med.voll.api.domain.paciente.DatosActualizacionPaciente;
 import med.voll.api.domain.paciente.DatosDetallePaciente;
@@ -26,6 +27,7 @@ import med.voll.api.domain.paciente.Paciente;
 
 @RestController
 @RequestMapping("/pacientes")
+@SecurityRequirement(name = "bearer-key")
 public class PacienteController {
 
     @Autowired
