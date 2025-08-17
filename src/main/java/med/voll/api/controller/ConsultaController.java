3ea881154b91713c379af81884eecc771798af06
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-08-16 19:58:05 -0600

    add class service

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,15 +11,21 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.domain.consulta.DatosDetalleConsulta;
 import med.voll.api.domain.consulta.DatosReservaConsulta;
+import med.voll.api.domain.consulta.ReservaDeConsultas;
 
 @RestController
 @RequestMapping("consultas")
 public class ConsultaController {
 
+    @Autowired
+    private ReservaDeConsultas reserva;
+
     @PostMapping
     @Transactional
     public ResponseEntity reservar(@RequestBody @Valid DatosReservaConsulta datos){
-        System.out.println(datos);
+        
+        reserva.reservar(datos);
+
         return ResponseEntity.ok(new DatosDetalleConsulta(null, null, null, null));
     }
 }
---
+++ src/main/java/med/voll/api/domain/consulta/ReservaDeConsultas.java
@@ -0,0 +1,31 @@
+package med.voll.api.domain.consulta;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.paciente.PacienteRepository;
+
+@Service
+public class ReservaDeConsultas {
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    @Autowired
+    private ConsultaRepository consultarepository;
+
+    public void reservar(DatosReservaConsulta datos){
+
+
+        var medico = medicoRepository.findById(datos.idMedico()).get();
+        var paciente = pacienteRepository.findById(datos.idPaciente()).get();
+        
+        var consulta = new Consulta(null, medico, paciente, datos.fecha()); 
+
+        consultarepository.save(consulta);
+    }
+}
