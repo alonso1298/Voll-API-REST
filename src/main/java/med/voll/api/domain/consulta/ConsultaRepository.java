3d688a02594a8d89a6c6dee4d3fd80e3edc2d4b9
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-08-16 22:34:21 -0600

    add ValidadorMedicoConOtraConsultaEnElMismoHorario

--- src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -4,11 +4,11 @@ import java.time.LocalDateTime;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import jakarta.validation.constraints.NotNull;
-
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
 
     boolean existsByPacienteeIdAndFechaBetween(Long idPaciente, LocalDateTime primerHorario,
             LocalDateTime ultimoHorario);
+
+    boolean existsByMedicoIdAndFecha(Long idMedico, LocalDateTime fecha);
 
 }
