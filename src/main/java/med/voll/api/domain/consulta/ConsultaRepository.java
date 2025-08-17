40680f28074cc1612fc14b6227d818a3dbc941cc
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-08-16 22:26:12 -0600

    add ValidadorPacienteSinOtraConsultaEnElMismoDia

--- src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -1,7 +1,14 @@
 package med.voll.api.domain.consulta;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import jakarta.validation.constraints.NotNull;
+
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+
+    boolean existsByPacienteeIdAndFechaBetween(Long idPaciente, LocalDateTime primerHorario,
+            LocalDateTime ultimoHorario);
 
 }
