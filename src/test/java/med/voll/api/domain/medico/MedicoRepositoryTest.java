7d5f7c5e6f8b87655855f61f363abbb08920f58e
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-08-17 21:59:29 -0600

    Testeando Repository

--- src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
+++ src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
@@ -1,16 +1,43 @@
 package med.voll.api.domain.medico;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+import javax.swing.text.html.parser.Entity;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import jakarta.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 public class MedicoRepositoryTest {
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private EntityManager em;
+
     @Test
-    void testElegirMedicoAleatorioDisponibleEnLaFecha() {
-        
+    @DisplayName("Deberia devolver null cuando el medico buscado existe pero no esta disponible")
+    void testElegirMedicoAleatorioDisponibleEnLaFechaEscenario1() {
+        var lunesSiguienteALas10 = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.MONDAY)).atTime(10, 0);
+
+        var medico = 
+        var paciente = 
+        var consulta = 
+
+        var medicoLibre = medicoRepository.elegirMedicoAleatorioDisponibleEnLaFecha(Especialidad.CARDIOLOGIA, lunesSiguienteALas10);
+        assertThat(medicoLibre).isNull();
     }
 }
